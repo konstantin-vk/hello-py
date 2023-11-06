b1c923f24de88fbad751617b4584a89dd6b15c35
konstantin-vk <konstantin.krasilnik@gmail.com>
2023-11-06 22:09:23 +0200

    -

--- course-py-full-course/11-19-datatypes/drill.py
+++ course-py-full-course/11-19-datatypes/drill.py
@@ -1,3 +1,4 @@
+##########################################
 print ("STRING")
 some_str = """1
 2
@@ -10,19 +11,19 @@ print ( some_str.index("1")) #error wjhen not cound : ValueError: substring not 
 some_str1 = some_str.replace("1",'2')
 print (some_str)
 
-
+#############################################
 print ("INT")
 i = int("2")
 print (i)
 print(pow(1,3))
 
-
+#############################################
 print ("FLOAT")
 f = 1.7
 print (f)
 print (int(f))
 
-
+##################################################
 print ("COMPLEX")
 complex_a = 3+1j
 complex_b = 4+2j
@@ -30,17 +31,17 @@ print (complex_a + complex_b)
 print (complex_a *  complex_b) 
 
 
-
+##################################################
 print ("BOOL")
 is_true = True 
 print (not is_true)
 print (bool(True))
 print (bool(is_true))
 print (bool(1>2))
-
+################################################
 print ("TYPE CONVERSION")
 
-
+###############################################
 print ("LIST")
 users = [
     {
@@ -54,3 +55,87 @@ users = [
     }
 ]
 print (users[1]['name'])
+###########################################################
+print ('LIST')
+my_numbers = [0, 10, 20, 30, 40]
+print( dir(my_numbers))
+my_numbers.append(10)
+print ( my_numbers.count(10) )
+my_numbers.insert(0,-1)
+print (my_numbers)
+my_numbers.clear()
+print (my_numbers)   ## []
+print (my_numbers.extend('abc')) ## [a,b,c] !!!!
+print (my_numbers)
+
+old_list = [1,2,3]
+new_list_as_copy = old_list
+print ( id (new_list_as_copy) == id(old_list)) ## = true 
+new_list_as_new = old_list.copy()
+print ( id (new_list_as_new) == id(old_list)) ## = false
+
+##############################################################
+print ("PRACTICAL PART")
+# create list with 5 types 
+# int float complex str bool 
+lst = [1, 1.0 , 2+1j , "STR" , True]
+# delete 3d 
+del lst[3]  # !!!
+print(type(2+1j))
+# print len 
+print (len(lst))
+# reverese 
+print (lst.reverse())
+# new list with 2 elem 
+lst2 = [1,2]
+# extend first with all elemennts of the second one 
+# lst.append(lst2)  nonon = [True, (2+1j), 1.0, 1, [1, 2]]
+lst = lst + lst2 # yes yes yes
+# print then 
+print (lst)
+
+#############################################################################3
+print ("DICT")
+car = {
+    'brand': 'Chevy' , 
+    'brand': 'Chevrolet' ,  # overwrite previous value 
+    'price': 10000 , 
+    'engine_vol': 1.4 ,     # comma is allowed 
+}
+print (car)
+print ( car.get('brand') )
+print (  car['brand'] )
+car['brand_logo'] = 'Chevy'
+
+
+
+car = {
+    'brand': 'Chevy' , 
+    'brand': 'Chevrolet' ,  # overwrite previous value 
+    'price': 10000 , 
+    'engine': {
+        'vol': 1.4 ,      
+        'type': 'gas' ,
+    }
+}
+
+print ( car['engine']['type'] ) 
+##########################################################################
+
+print ("DICT practicval")
+# crate empty dict 
+dct = {}
+# 3 times ask to enter key-calue 
+for i in range(3):
+    key = input("Enter key")
+    value = input("Enter value" )
+    # add values one by one 
+    dct[key]=value
+# print 1
+print(dct)
+
+
+#######################################
+print("TUPLE")
+#######################################
+print ("SET")
--- course-py-full-course/11-19-datatypes/readme.md
+++ course-py-full-course/11-19-datatypes/readme.md
@@ -58,7 +58,7 @@ set()
         // error if impossible to convert 
 
 
-# LIST (array)
+# LIST (array \ списки)
 
 список - упорядоченная последовательность символов.
 fruits = ['apple','grape','banana']
@@ -81,6 +81,8 @@ users = [
 ]
 print (usersp[1]['name'])
 
+## List methods 
+
 append()
 pop()
 remove()
@@ -93,5 +95,125 @@ copy()
 extend()
 reverse()
 countn()
+
+
+
+## Convert to list 
+  = list()
+
+greeting = "Hello hello"
+letters = list(greeting)
+
+## List Functions 
+min( mylist )
+max( mylist )
+sum()
+len() 
+list = list1 + list2 = [ LIKE UNION ALL ]  ie tethod __add__
+
+## List to list 
+new_list = old_list[0:1]   old_list[:2]   old_list[-2:] ... [:]
+
+
+## Copying list 
+new_list = old_list   == it is the same list !!!!
+    id(new_list) = id(old_list)
+
+correct ways 
+new_list = old_list[:]
+new_list = old_list.copy()
+new_list = list(old_list)
+
+
+## LIST SET TUPLE 
+List -> SET mutable -> TUPLE immutable 
+
+
+List	                                    | Set	                                | Tuple
+-------------------------------------------------------------------------------------------------
+Lists is Mutable	                        | Set is Mutable	                    | Tuple is Immutable
+It is Ordered collection of items	        | It is Unordered collection of items	| It is Ordered collection of items
+Items in list can be replaced or changed	| Items in set cannot be changed or replaced but you can remove and add new items.	| Items in tuple cannot be changed or replaced
+
+
+
+
+
+# DICT   словарь
+
+car = {
+    'brand': 'Chevy' , 
+    'brand': 'Chevrolet' ,  # overwrite previous value 
+    'price': 10000 , 
+    'engine_vol': 1.4 ,     # comma is allowed 
+}
+
+## DICT compare 
+    ==    = compares by components 
+
+## DICT get & change 
+car['brand']
+car.get('brand')
+car['brand_logo'] = 'Chevy' = add new key 
+
+## DICT variables in a dict 
+brand_key_name = 'brand'
+car[brand_key_name]
+
+## DICT multilevel 
+car = {
+    'brand': 'Chevy' , 
+    'brand': 'Chevrolet' ,  # overwrite previous value 
+    'price': 10000 , 
+    'engine': {
+        'vol': 1.4 ,      
+        'type': 'gas' ,
+    }
+}
+
+car['engine']['type']
+
+## DICT length 
+len( car )   => number of keys 
+
+## DICT delete key 
+del car['price']
+
+
+## DICT noe existent ekys and get 
+cal['model']   -> error 
+car.get('model')  -> None = no error
+car.get('model','default')  -> default  = no error
+
+print ( car.__doc__ ) 
+
+car.items()   == return dict_items - class 
+car.keys()    == dict_keys 
+car.popitem () 
+
+
+---
+# TUPLE (кортеж)
+!!! unmutable 
+
+my_tuple = (1.0,2,'three')
+кортеж словарей , списков и тд - словарь менять можно !!! 
+users = (
+    {
+        'user_id': '0'
+        'user_name': 'root',
+    }
+)
+users[0]['user_id'] = '-1'  -> OK !!! 
+
+
+## TUPLE methods
+count 
+index 
+
+
+# SET набор 
+{}  -> like dict , but no key:value 
+union (unique elements)
 
 
